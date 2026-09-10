5ea18e80c181d18cca8958001db36e3a9e9dcdd2
debstarnetwork-a11y <debstarnetwork@gmail.com>
2026-09-10 09:22:45 +0100

    refactor: remove data recovery component and add SPA routing
    Removed the legacy data recovery utility and configured Vercel rewrites to support client-side routing for the Single Page Application.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { BankProvider } from './store.tsx';
 import { LanguageProvider } from './context/LanguageContext.tsx';
-import { DataRecovery } from './components/DataRecovery.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -11,7 +10,6 @@ createRoot(document.getElementById('root')!).render(
     <BankProvider>
       <LanguageProvider>
         <App />
-        <DataRecovery />
       </LanguageProvider>
     </BankProvider>
   </StrictMode>,
